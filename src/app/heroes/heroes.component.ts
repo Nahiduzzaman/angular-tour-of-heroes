1bcee4198064d3defd0c5517cdd1c73a93a29883
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2018-04-01 01:42:43 +0600

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
-
-@Component({
-    selector: 'app-heroes',
-    templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css']
-})
+@Component({selector: 'app-heroes', templateUrl: './heroes.component.html', styleUrls: ['./heroes.component.css']})
 export class HeroesComponent implements OnInit {
-    heroes = HEROES;
+    heroes : Hero[];
 
-    selectedHero: Hero;
-
-    constructor() { }
-
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
+    constructor(private heroService : HeroService) {}
 
     ngOnInit() {
+        this.getHeroes();
     }
 
+    getHeroes() : void {
+        this
+            .heroService
+            .getHeroes()
+            .subscribe(heroes => this.heroes = heroes);
+    }
 }

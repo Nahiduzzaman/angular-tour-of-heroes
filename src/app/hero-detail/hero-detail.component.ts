7afd040bd209aebed5f6f67117f3980d3c4d0ca9
SELISE\nahid.uzzaman <nahid.uzzaman@selise.ch>
2018-04-01 19:58:24 +0600

    update

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Hero } from '../hero';
-import { HeroService }  from '../hero.service';
+import { HeroService } from '../hero.service';
 
 @Component({
     selector: 'app-hero-detail',
@@ -18,6 +18,22 @@ export class HeroDetailComponent implements OnInit {
         private location: Location
     ) { }
 
-    ngOnInit() {
+    ngOnInit(): void {
+        this.getHero();
+    }
+
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id');
+        this.heroService.getHero(id)
+            .subscribe(hero => this.hero = hero);
+    }
+
+    save(): void {
+        this.heroService.updateHero(this.hero)
+            .subscribe(() => this.goBack());
+    }
+
+    goBack(): void {
+        this.location.back();
     }
 }

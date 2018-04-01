dcd4fc6702c9086a30fc933ee2f6b2a1429e4912
DESKTOP-2OU1GPU\nahid <nahiduzzaman@sgcsoft.net>
2018-04-02 01:17:25 +0600

    update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,18 +21,26 @@ export class HeroService {
         private messageService: MessageService
     ) { }
 
-
-    /** Log a HeroService message with the MessageService */
-    private log(message: string) {
-        this.messageService.add('HeroService: ' + message);
-    }
-
     /** GET heroes from the server */
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(this.heroesUrl)
             .pipe(
                 tap(heroes => this.log(`fetched heroes`)),
                 catchError(this.handleError('getHeroes', []))
+            );
+    }
+
+    /** GET hero by id. Return `undefined` when id not found */
+    getHeroNo404<Data>(id: number): Observable<Hero> {
+        const url = `${this.heroesUrl}/?id=${id}`;
+        return this.http.get<Hero[]>(url)
+            .pipe(
+                map(heroes => heroes[0]), // returns a {0|1} element array
+                tap(h => {
+                    const outcome = h ? `fetched` : `did not find`;
+                    this.log(`${outcome} hero id=${id}`);
+                }),
+                catchError(this.handleError<Hero>(`getHero id=${id}`))
             );
     }
 
@@ -44,10 +52,27 @@ export class HeroService {
         );
     }
 
+    /** POST: add a new hero to the server */
+    addHero (hero: Hero): Observable<Hero> {
+        return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+            tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+            catchError(this.handleError<Hero>('addHero'))
+        );
+    }
+
     updateHero(hero: Hero): Observable<any> {
         return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
             tap(_ => this.log(`updated hero id=${hero.id}`)),
             catchError(this.handleError<any>('updateHero'))
+        );
+    }
+
+    deleteHero (hero: Hero | number): Observable<Hero> {
+        const id = typeof hero === 'number' ? hero : hero.id;
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete<Hero>(url, httpOptions).pipe(
+            tap(_ => this.log(`deleted hero id=${id}`)),
+            catchError(this.handleError<Hero>('deleteHero'))
         );
     }
 
